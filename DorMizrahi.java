65ff44fc45418933750c926ad633f72fd6c37959
DorMizrahii <dormizrahi1209@gmail.com>
2023-07-16 20:18:38 +0300

    Commited for exercise 3

--- DorMizrahi.java
+++ DorMizrahi.java
@@ -1,7 +1,7 @@
-public class Hello {
+public class DorMizrahi {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("Hello World..." + i);
+			System.out.println("Hello My Name is Dor" + i);
 		}
 	}
 }
